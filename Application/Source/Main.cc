03e6d66faa0ee40c2605aa3c42c655a8242df6df
Jaclaru@すこん部 <h879822739@gmail.com>
2025-09-04 22:31:21 +0800

    更新 CMake 配置，启用 spdlog 动态链接
    - 在顶层 `CMakeLists.txt` 的 `else()` 部分添加 `add_compile_definitions(SPDLOG_COMPILED_LIB)`启用动态链接。
    - 更新 `Renderer` 的链接库，添加对 `spdlog` 的链接。

--- Application/Source/Main.cc
+++ Application/Source/Main.cc
@@ -3,21 +3,16 @@
 
 #include <QApplication>
 
-// 让spdlog使用动态链接，减小体积
-#define SPDLOG_COMPILED_LIB
 #include <spdlog/spdlog.h>
 
 int main(int Argc, char** Argv)
 {
-    // 设置系统本地化信息，防止中文乱码
-    setlocale(LC_ALL, "");
-
     QApplication QApp(Argc, Argv);
 
     SilverBell::Application::FApplication Application;
     Application.show();
 
-
+    // 由于编译的spdlog动态库勾选了宽字符和utf-8支持，所以可以直接打印中文
     spdlog::info("中文日志测试：{}", "你好，世界！");
 
     return QApp.exec();
